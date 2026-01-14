e3436947f8fc477e0d407f57477f8e9a267a0680
gr4yx01 <hoyx0101@gmail.com>
2026-01-14 15:03:28 +0100

    feat: implement controllers

--- src/main/java/com/graey/spring_web/HomeController.java
+++ src/main/java/com/graey/spring_web/HomeController.java
@@ -1,13 +1,29 @@
 package com.graey.spring_web;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping
+    @RequestMapping("/")
     public String home() {
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req, HttpSession session) {
+        int numOne = Integer.parseInt(req.getParameter("num1"));
+        int numTwo = Integer.parseInt(req.getParameter("num2"));
+
+        int result = numOne + numTwo;
+
+        session.setAttribute("result", result);
+
+//        System.out.println(result);
+
+        return "result.jsp";
     }
 }
---
+++ src/main/webapp/result.jsp
@@ -0,0 +1,7 @@
+<%@page language="java" %>
+
+<html>
+<body>
+    <h2>Result: </h2>
+</body>
+</html>
